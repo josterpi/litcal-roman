239b2d208cc3970195176fca53f51023f4191009
Jeremy Osterhouse <josterhouse@gmail.com>
2026-05-04 13:57:12 -0400

    Add __init__ for public API

--- src/litcal_roman/__init__.py
+++ src/litcal_roman/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from litcal-roman!"
+from litcal_roman.calendar import get_liturgical_day
+from litcal_roman.models import CalendarConfig
+from litcal_roman import models
+
+__all__ = ["get_liturgical_day", "CalendarConfig", "models"]
